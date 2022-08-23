dc67c8603b40209300a427d33e96926a5c5ddb31
김경묵 <apsode211@naver.com>
2022-08-23 16:18:48 +0900

    수정

--- src/Route/Detail.js
+++ src/Route/Detail.js
@@ -16,9 +16,6 @@ function Detail() {
 
     let arr = []
     let newarr = arr.concat(a.dress, a.top, a.bottom, a.auter)
-    // console.log(newarr);
-
-    // let [item, setItem] = useState(All())
 
     let navigate = useNavigate();
     console.log(page);
@@ -36,7 +33,25 @@ function Detail() {
                 </div>
                 <div className={cs('cart')}>
                     <p>{aa[0].title}</p>
+                    <br></br>
                     <p>{aa[0].price}</p>
+                    <br></br>
+                    <div className={cs('shipping')}>
+                        <div className={cs('ship-box1')}>
+                            <p>배송방법</p>
+                            <p>배송비</p>
+                        </div>
+
+                        <div className={cs('ship-box2')}>
+                            <p>택배</p>
+                            <select>
+                                <option value="">주문시 결제(선결제)</option>
+                                <option value="">수령시 결제(착불)</option>
+                            </select>
+                            <p style={{ fontSize: '13px', marginLeft: '0px' }}>0원 (OPEN기념 무료 배송)</p>
+
+                        </div>
+                    </div>
                 </div>
             </>
         )
