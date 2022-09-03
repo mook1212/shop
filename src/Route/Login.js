957d31825968754d2798d1c9b44549011e875870
김경묵 <apsode211@naver.com>
2022-09-03 23:00:21 +0900

    회원가입 기능 수정

--- src/Route/Login.js
+++ src/Route/Login.js
@@ -5,32 +5,114 @@ import { createContext, useState } from "react"
 import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom'
 const cs = classNames.bind(style);
 
+
 function Login() {
 
+    let [join, setJoin] = useState('none')
+
     return (
-        <div className={cs('main')}>
-            <div className={cs('login-box')}>
-                <h1>LOGIN</h1>
-                <div className={cs('content')}>
-                    {/* <form> */}
+        <div>
+            <div className={cs('main')}>
+                <div className={cs('login-box')}>
+                    <h1>LOGIN</h1>
+                    <div className={cs('content')}>
                         <input type='text' placeholder='아이디를 입력하세요.' />
                         <input type='text' placeholder='비밀번호를 입력하세요.' />
-                    {/* </form> */}
-                </div>
+                    </div>
 
-                <div className={cs('login-btn')}>
-                    <button>LOGIN</button>
-                    <button>JOIN US</button>
-                </div>
+                    <div className={cs('login-btn')}>
+                        <button id='login' onClick={() => {
 
-                <div className='flex' style={{ marginTop: '10px' }}>
-                    <p style={{ fontSize: '13px', marginRight: '10px' }}>* 아이디 찾기</p>
-                    <p style={{ fontSize: '13px' }}>* 비밀번호 찾기</p>
+                        }}>LOGIN</button>
+                        <button id='joinus' onClick={() => {
+                            if (join == 'none') {
+                                setJoin('show')
+                            } else if (join == 'show') {
+                                setJoin('none')
+                            }
+                        }}>JOIN US</button>
+                    </div>
+
+                    <div className='flex' style={{ marginTop: '10px' }}>
+                        <p style={{ fontSize: '13px', marginRight: '10px' }}>* 아이디 찾기</p>
+                        <p style={{ fontSize: '13px' }}>* 비밀번호 찾기</p>
+                    </div>
+                </div>
+            </div>
+
+            <div id='dropdown-nav' className={cs(`dropdown-nav`, `${join}`)}>
+                <div className={cs('nav-modal')}>
+                    <p>회원가입</p>
+                    <form>
+                        <div className={cs('id-box')}>
+                            <p>아이디 *</p>
+                            <input id='join-id' type='text' />
+                        </div>
+
+                        <div className={cs('pw-box')}>
+                            <p>패스워드 *</p>
+                            <input id='join-pw' type='text' />
+                        </div>
+                        <div>
+                            <p>이름</p>
+                            <input id='join-name' type='text' />
+                        </div>
+                        <div>
+                            <p>전화번호</p>
+                            <input id='join-phone' type='text' />
+                        </div>
+                    </form>
+
+                    <div className={cs('join-box')}>
+                        <button onClick={() => {
+
+                            let id = document.querySelector('#join-id').value;
+                            let pw = document.querySelector('#join-pw').value;
+                            let name = document.querySelector('#join-name').value;
+                            let phone = document.querySelector('#join-name').value;
+
+                            window.localStorage.setItem('ID', id)
+                            window.localStorage.setItem('PW', pw)
+
+                            if (id == '') {
+                                alert('아이디를 입력해주세요.')
+                            }
+                            else if (pw == '') {
+                                alert('패스워드를 입력해주세요.')
+                            }
+                            else if (name == '') {
+                                alert('이름을 입력해주세요.')
+                            }
+                            else if (phone == '') {
+                                alert('전화번호를 입력해주세요.')
+                            } else {
+                                if (join == 'none') {
+                                    setJoin('show')
+                                } else if (join == 'show') {
+                                    setJoin('none')
+                                }
+                            }
+                        }}>회원가입</button>
+                        <button onClick={() => {
+                            if (join == 'none') {
+                                setJoin('show')
+                            } else if (join == 'show') {
+                                setJoin('none')
+                            }
+                        }}>돌아가기</button>
+
+                    </div>
                 </div>
             </div>
         </div>
 
     )
+}
+
+function Join() {
+
+    // document.querySelector('#join-id')
+
 }
 
 export default Login
